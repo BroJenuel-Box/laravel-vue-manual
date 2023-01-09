dbc552df7814515a63ba596dca466f047b8d5b97
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-01-09 16:38:16 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/app/{any}', function () {
+    $path = public_path('app/index.html');
+    abort_unless(file_exists($path), 400, 'Page is not Found!');
+    return file_get_contents($path);
+})
+    ->name('FrontEndApp');
